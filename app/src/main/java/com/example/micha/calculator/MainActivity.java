6b79d05444a99fd5020024f5ccf57ce8a63c2442
Mazogu <michael_doom@hotmail.com>
2018-01-11 02:30:02 -0500

    basic operations work now

--- app/src/main/java/com/example/micha/calculator/MainActivity.java
+++ app/src/main/java/com/example/micha/calculator/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.micha.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int argumentOne, argumentTwo;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private double argumentOne, argumentTwo;
     private String operation;
     private TextView answer;
     private TextView math;
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String edit = math.getText().toString();
-                if(!edit.contains(".")){
+                if (!edit.contains(".")) {
                     math.setText(edit + ".");
                 }
             }
@@ -46,6 +48,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if(!operation.equals("")) {
+                    String number = math.getText().toString().split(" ")[2];
+
+                    argumentTwo = Double.parseDouble(number);
+                    switch(operation){
+                        case "+":
+                            answer.setText(Double.toString(argumentOne + argumentTwo));
+                            break;
+                        case "x":
+                            answer.setText(Double.toString(argumentOne * argumentTwo));
+                            break;
+                        case "÷":
+                            answer.setText(Double.toString(argumentOne / argumentTwo));
+                            break;
+                        case "-":
+                            answer.setText(Double.toString(argumentOne - argumentTwo));
+                            break;
+                        default:
+                            Log.d(TAG, "Not sure what happened: " + operation);
+                    }
+                    operation = "";
+                    argumentOne = argumentTwo = 0;
+                    math.setText(getString(R.string.zero_string));
+                }
             }
         });
 
@@ -53,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 math.setText(getString(R.string.zero_string));
+                operation = "";
+                argumentOne = 0;
             }
         });
 
@@ -70,24 +98,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String number = math.getText().toString();
                 double value = Double.parseDouble(number);
-                math.setText(Double.toString(value*value));
+                math.setText(Double.toString(value * value));
             }
         });
     }
 
     public void addNumber(View view) {
-        if(operation.equals("")){
-            Button pressed = (Button) view;
-            if(math.getText().toString().equals("0")){
-                math.setText(pressed.getText().toString());
-            }
-            else {
-                math.setText(math.getText().toString() + pressed.getText().toString());
-            }
+        String number = math.getText().toString();
+        Button pressed = (Button) view;
+        if (number.equals("0")) {
+            math.setText(pressed.getText().toString());
+        } else {
+            math.setText(number + pressed.getText().toString());
         }
+
     }
 
 
-
-
+    public void setOperation(View view) {
+        if (operation.equals("")) {
+            argumentOne = Double.parseDouble(math.getText().toString());
+            operation = ((Button) view).getText().toString();
+            math.setText(math.getText().toString()+" "+operation+" ");
+        }
+    }
 }
