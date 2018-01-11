b7815ac2e6d70ff356bbe828413456c9043fff85
Mazogu <michael_doom@hotmail.com>
2018-01-10 23:36:04 -0500

    number buttons work

--- app/src/main/java/com/example/micha/calculator/MainActivity.java
+++ app/src/main/java/com/example/micha/calculator/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.micha.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int argumentOne, argumentTwo;
+    private String operation;
+    private TextView answer;
+    private TextView math;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        argumentOne = argumentTwo = 0;
+        math = findViewById(R.id.math);
+        answer = findViewById(R.id.answer);
+        operation = "";
+
+    }
+
+    public void addNumber(View view) {
+        if(operation.equals("")){
+            Button pressed = (Button) view;
+            if(math.getText().toString().equals("0")){
+                math.setText(pressed.getText().toString());
+            }
+            else {
+                math.setText(math.getText().toString() + pressed.getText().toString());
+            }
+        }
     }
 }
