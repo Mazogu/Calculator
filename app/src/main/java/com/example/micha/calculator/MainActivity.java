158d1736fe004a4f263df00523ecceda00b686bd
Mazogu <michael_doom@hotmail.com>
2018-01-11 03:06:24 -0500

    added an new layout for horizontal orientation with more buttons

--- app/src/main/java/com/example/micha/calculator/MainActivity.java
+++ app/src/main/java/com/example/micha/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
 
